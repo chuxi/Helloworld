8053c59a076fcccb7321e08f7fd29b9ef466a1a6
chuxi <chuxiking@gmail.com>
2013-09-30 15:13:51 +0800

    Create hello2.java
    commit of hello2.java

---
+++ hello2.java
@@ -0,0 +1,12 @@
+public class hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world 2!");
+		int a=132;
+		int b=~a;
+		System.out.println(b);
+		System.out.println("version change control!");
+	}
+
+}
